f5d40fa701620d44384cc0220be185ca19a992a9
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-03-16 19:45:36 -0400

    started code for studentmanager.java by student3, Jaskaran Singh and added step 1 Created ArrayList to store student objects

--- StudentManager.java
+++ StudentManager.java
@@ -1,3 +1,12 @@
+// =======================================================
+
+// Code by: Jaskaran Singh
+// Role: Collections (ArrayList) & Data Processing
+// =======================================================
+
+import java.util.ArrayList;
+
 public class StudentManager {
-    
-}
+
+    // Step 1: Created ArrayList to store student objects
+    private ArrayList<Student> students = new ArrayList<>();
