09a7d0a73279cb85b071763ede3ade38f9d20273
avipatel1226 <avipatel1226@gmail.com>
2026-03-16 19:31:26 -0400

    Added Instructor class extending Person to support inheritance structure

--- Instructor.java
+++ Instructor.java
@@ -1,3 +1,13 @@
-public class Instructor {
-    
-}
+// =======================================================
+// by  Avi Patel
+// Role: Demonstrating Polymorphism
+// =======================================================
+
+public class Instructor extends Person {
+
+    private String department;
+
+    public Instructor(String name, String id, String email, String department) {
+        super(name, id, email);
+        this.department = department;
+    }
