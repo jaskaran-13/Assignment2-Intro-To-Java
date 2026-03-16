d86d353a12b8c8a119418a69afcc46184d105b46
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-03-16 19:49:58 -0400

    then in Main.Java Step 1 added  to input name of the student

--- Main.java
+++ Main.java
@@ -1,3 +1,23 @@
+// =======================================================
+//  Avi Patel(student1), Jaswinder Singh(Student2), Jaskaran Singh(Student3)
+// Role: Integration of all modules
+// =======================================================
+
+import java.util.Scanner;
+
 public class Main {
-    
-}
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        StudentManager manager = new StudentManager();
+
+        try {
+
+            // Step 1: Input Student Name
+            System.out.print("Enter Name of the student: ");
+            String name = scanner.nextLine();
+
+            if (!InputValidator.validateName(name)) {
+                throw new Exception("Invalid Name Format");
+            }
