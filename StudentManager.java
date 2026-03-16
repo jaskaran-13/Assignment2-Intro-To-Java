b59f15bee95dd4e1701491f20b83ddac9ea61d1b
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-03-16 19:46:36 -0400

    added step 2 and 3 to add student and remove student by ID

--- StudentManager.java
+++ StudentManager.java
@@ -10,3 +10,12 @@ public class StudentManager {
 
     // Step 1: Created ArrayList to store student objects
     private ArrayList<Student> students = new ArrayList<>();
+ // Step 2: Add Student
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    // Step 3: Remove Student by ID
+    public void removeStudent(String id) {
+        students.removeIf(student -> student.getId().equals(id));
+    }
