f3eab24edff87992a7fe879d899c16d5cefa162a
avipatel1226 <avipatel1226@gmail.com>
2026-03-16 19:23:36 -0400

    Added Student class extending Person with constructor and course attribute

--- Student.java
+++ Student.java
@@ -8,3 +8,9 @@
 public class Student extends Person {
 
     private String course;
+
+    // Step 2:  created Constructor
+    public Student(String name, String id, String email, String course) {
+        super(name, id, email);
+        this.course = course;
+    }
