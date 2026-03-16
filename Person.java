cb9a31b9e9b3ca2726aa9dd37c0dba246019088e
avipatel1226 <avipatel1226@gmail.com>
2026-03-16 19:11:06 -0400

    Added Person class implementation with encapsulation and constructor

--- Person.java
+++ Person.java
@@ -1,3 +1,16 @@
-public class Person {
-    
-}
+// =======================================================
+// Code By: Avi Patel
+//  Inheritance Implementation
+// =======================================================
+
+// Step 1: Student class extends Person class
+
+public class Student extends Person {
+
+    private String course;
+
+    // Step 2:  created Constructor
+    public Student(String name, String id, String email, String course) {
+        super(name, id, email);
+        this.course = course;
+    }
