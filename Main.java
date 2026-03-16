0cda51c03346a423bdde373e1af0c1ae26dfbfe5
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-03-16 19:53:42 -0400

    then added step 5,6 and 7

--- Main.java
+++ Main.java
@@ -32,3 +32,12 @@ public class Main {
             // Step 4: Enter name of the Course
             System.out.print("Enter Course: ");
             String course = scanner.nextLine();
+             // Step 5: Create Student Object
+            Student student = new Student(name, id, email, course);
+
+            // Step 6: Add to ArrayList
+            manager.addStudent(student);
+
+            // Step 7: Display Students
+            manager.displayStudents();
+
